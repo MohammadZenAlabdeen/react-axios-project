01cc4ab58ab18b8323e9c2a1c502e86e150f200a
MohammadZenAlabdeen <myn1ytc@gmail.com>
2024-02-08 16:50:07 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base:"./books-website-with-axios-and-react-hooks",
+  base:"/react-axios-project",
   plugins: [react()],
   optimizeDeps: {
     include: ['@emotion/styled'],
