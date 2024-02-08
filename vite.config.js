97eba7ceaed41cc5f7947974dd3324e3a1a14001
MohammadZenAlabdeen <myn1ytc@gmail.com>
2024-02-08 17:08:44 +0300

    ad

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base:"/react-axios-project",
+  base:"/react-axios-project/",
   plugins: [react()],
   optimizeDeps: {
     include: ['@emotion/styled'],
